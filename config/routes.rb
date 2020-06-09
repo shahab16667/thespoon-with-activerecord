3f4abe0f2e37225d47d8ff775bdd85b12e380c22
Shahab Jahangiri <shahab.j3@gmail.com>
2020-06-09 17:12:23 +1000

    commented the manual routing and added resources :restaurants to use rails prefix routing style

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/restaurants', to: 'restaurant#index'
-  get '/restaurants/:id', to: 'restaurants#show'
-  get '/restaurants/new', to: 'restaurant#new'
-  post '/restaurants', to: 'restaurants#create'
-  get '/restaurant/:id/edit', to: 'restaurant#edit'
-  patch '/restaurants/:id', to:  'restaurants#update'
-  delete '/restaurants/:id', to: 'restaurants#destroy'
+  # get '/restaurants', to: 'restaurant#index'
+  # get '/restaurants/:id', to: 'restaurants#show'
+  # get '/restaurants/new', to: 'restaurant#new'
+  # post '/restaurants', to: 'restaurants#create'
+  # get '/restaurant/:id/edit', to: 'restaurant#edit'
+  # patch '/restaurants/:id', to:  'restaurants#update'
+  # delete '/restaurants/:id', to: 'restaurants#destroy'
+  resources :restaurants
 end
