59dbde5200e29219a564a10934cd58c4dbf71606
Shahab Jahangiri <shahab.j3@gmail.com>
2020-06-09 21:20:22 +1000

    7 CRUD Action done and all works

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,7 +14,7 @@ class RestaurantsController < ApplicationController
     def create
         @restaurant = Restaurant.new(restaurant_params)
         @restaurant.save
-        redirect_to restaurant_path(@restaurant.id)
+        redirect_to restaurant_path(@restaurant)
     end
 
     def edit
@@ -23,11 +23,15 @@ class RestaurantsController < ApplicationController
     end
 
     def update
-
+        @restaurant = Restaurant.find(params[:id])
+        @restaurant.update(restaurant_params)
+        redirect_to restaurant_path(@restaurant)
     end
 
     def destroy
-
+        @restaurant = Restaurant.find(params[:id])
+        @restaurant.destroy
+        redirect_to restaurant_path
     end
 
     private
